5f6f5f42950e241eb84a9514841aea543850c3ce
daqh <valeriodipasq@gmail.com>
2023-12-01 12:59:18 +0100

    Add Azure SQL Database setup script

--- environment.sh
+++ environment.sh
@@ -6,8 +6,28 @@ PROJECT_NAME="my-project"
 LOCATION="italynorth"
 RESOURCE_GROUP_SUFFIX="resource-group"
 
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# BEGIN: SQL Server variables
+SQL_SERVR_SUFFIX="sql-server"
+SQL_DATABASE_SUFFIX="database"
+SQL_SHADOW_DATABASE_SUFFIX="shadow"
+# ++++ CHANGE THESE VALUES ++++
+SQL_SERVER_ADMIN="sqladmin"
+SQL_SERVER_PASSWORD="P@ssw0rd"
+# END: SQL Server variables
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 # BEGIN: DO NOT MODIFY
-
 RESOURCE_GROUP="$PROJECT_NAME-$RESOURCE_GROUP_SUFFIX"
+SQL_SERVER="$PROJECT_NAME-$SQL_SERVR_SUFFIX"
+SQL_DATABASE="$PROJECT_NAME-$SQL_DATABASE_SUFFIX"
+SQL_SHADOW_DATABASE="$PROJECT_NAME-$SQL_DATABASE_SUFFIX-$SQL_SHADOW_DATABASE_SUFFIX"
 
+# Database URL for Prisma ORM and provider sqlserver (https://www.prisma.io/docs/reference/database-reference/connection-urls)
+DATABASE_URL="sqlserver://$SQL_SERVER.database.windows.net:1433;database=$SQL_DATABASE;user=$SQL_SERVER_ADMIN;password=$SQL_SERVER_PASSWORD"
+SHADOW_DATABASE_URL="sqlserver://$SQL_SERVER.database.windows.net:1433;database=$SQL_SHADOW_DATABASE;user=$SQL_SERVER_ADMIN;password=$SQL_SERVER_PASSWORD"
 # END: DO NOT MODIFY
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
--- install.sh
+++ install.sh
@@ -3,3 +3,33 @@
 # Create the resource group
 echo "Creating the resource group $RESOURCE_GROUP"
 az group create --resource-group $RESOURCE_GROUP --location $LOCATION
+
+# =========================
+# BEGIN: Azure SQL Database
+
+# Create the SQL Server
+echo "Creating the SQL Server $SQL_SERVER"
+az sql server create --location $LOCATION --resource-group $RESOURCE_GROUP --name $SQL_SERVER --admin-user $SQL_SERVER_ADMIN --admin-password $SQL_SERVER_PASSWORD
+
+# Create the firewall rule to allow all Azure IPs
+echo "Creating the firewall rule to allow all Azure IPs"
+az sql server firewall-rule create --resource-group $RESOURCE_GROUP --server $SQL_SERVER --start-ip-address "0.0.0.0" --end-ip-address "0.0.0.0" --name "AllowAllAzureIPs"
+
+# Create the firewall rule to allow your IP
+echo "Creating the firewall rule to allow your IP"
+az sql server firewall-rule create --resource-group $RESOURCE_GROUP --server $SQL_SERVER --start-ip-address "151.75.9.95" --end-ip-address "151.75.9.95" --name "AllowMyIP"
+
+# Create the database
+echo "Creating the database $SQL_DATABASE"
+az sql db create --resource-group $RESOURCE_GROUP --server $SQL_SERVER --name $SQL_DATABASE --free-limit true --free-limit-exhaustion-behavior AutoPause --edition GeneralPurpose --compute-model Serverless --family Gen5 --capacity 1
+
+# Create the shadow database (needed by Prisma ORM during migrations)
+echo "Creating the shadow database $SQL_SHADOW_DATABASE"
+az sql db create --resource-group $RESOURCE_GROUP --server $SQL_SERVER --name $SQL_SHADOW_DATABASE --edition GeneralPurpose --compute-model Serverless --family Gen5 --capacity 1 --zone-redundant false
+
+echo "" > .env
+echo "DATABASE_URL=\"$DATABASE_URL\"" >> .env
+echo "SHADOW_DATABASE_URL=\"$SHADOW_DATABASE_URL\"" >> .env
+
+# END: Azure SQL Database
+# =========================
