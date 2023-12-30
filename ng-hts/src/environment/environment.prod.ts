7bb5f7c2203d3cfa328e66068b9a4660b67d78ec
Valerio Di Pasquale <40913709+daqh@users.noreply.github.com>
2023-12-30 18:00:29 +0100

    Remove environment.prod.ts file

--- ng-hts/src/environment/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    apiBaseUrl: 'https://my-project-function-app.azurewebsites.net/api',
-    iotHubHostName: 'my-project-iot-hub.azure-devices.net',
-};
