9046d1ed8827f758f393bc498530afde15185466
Valerio Di Pasquale <40913709+daqh@users.noreply.github.com>
2024-01-17 20:34:46 +0100

    Update dashboard component and private component

--- ng-hts/src/app/private/dashboard/dashboard/dashboard.component.ts
+++ ng-hts/src/app/private/dashboard/dashboard/dashboard.component.ts
@@ -6,32 +6,6 @@ import { MsalService } from '@azure/msal-angular';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent implements OnInit {
-  constructor(private msalService: MsalService) {
-    msalService.initialize().subscribe((response) => {
-      console.log(response);
-    });
-  }
-
-  ngOnInit(): void {
-    console.log(this.msalService.instance.getActiveAccount());
-  }
-
-  login() {
-    this.msalService.loginPopup().subscribe((response) => {
-      console.log(response);
-      this.msalService.instance.setActiveAccount(response.account);
-    });
-  }
-
-  logout() {
-    this.msalService.logoutPopup().subscribe((response) => {
-      console.log(response);
-    });
-  }
-
-  isLoggedIn() {
-    return this.msalService.instance.getActiveAccount() != null;
-  }
+export class DashboardComponent {
 
 }
--- ng-hts/src/app/private/private.component.ts
+++ ng-hts/src/app/private/private.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MsalService } from '@azure/msal-angular';
 
 @Component({
   selector: 'app-private',
   templateUrl: './private.component.html',
   styleUrls: ['./private.component.css']
 })
-export class PrivateComponent {
+export class PrivateComponent implements OnInit {
+
+  constructor(
+    private msalService: MsalService,
+    private router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    console.log(this.msalService.instance.getActiveAccount());
+  }
+
+  logout() {
+    this.msalService.logout().subscribe((response) => {
+      console.log(response);
+      this.router.navigate(['/login']);
+    });
+  }
 
 }
