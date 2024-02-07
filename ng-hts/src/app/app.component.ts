5bf539542d79c4e19b3035261a9dd4aa9187c305
Valerio Di Pasquale <40913709+daqh@users.noreply.github.com>
2024-02-07 16:18:44 +0100

    Add MsalService initialization and environment logging in AppComponent

--- ng-hts/src/app/app.component.ts
+++ ng-hts/src/app/app.component.ts
@@ -1,8 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { MsalService } from '@azure/msal-angular';
+import { environment } from 'src/environment/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+
+  constructor(private msalService: MsalService) {
+    msalService.initialize();
+    console.log(environment)
+  }
+
+}
