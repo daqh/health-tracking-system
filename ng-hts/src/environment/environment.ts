d3cf670b3fd40f9f9d9adec6ec2bbd2c1f00ac33
Valerio Di Pasquale <40913709+daqh@users.noreply.github.com>
2024-02-11 12:45:47 +0100

    Remove unnecessary environment variables

--- ng-hts/src/environment/environment.ts
+++ ng-hts/src/environment/environment.ts
@@ -1,10 +1,3 @@
 export const environment = {
-	production: false,
-	iotHubHostName: "my-project-iot-hub.azure-devices.net",
-	deviceApiBaseUrl: "https://device-my-project-function-app.azurewebsites.net/api",
-	mealApiBaseUrl: "https://meal-my-project-function-app.azurewebsites.net/api",
-	msal: {
-		redirectUri: "http://localhost:4200",
-	},
-	ipinfoToken: "da60eeb3c2c5e5",
+
 }
